9c1a0b0c53c6acffeb7dcfcd59e16268390033e3
HoraceXIaoyiBao <47955018+HoraceXIaoyiBao@users.noreply.github.com>
2024-11-17 03:33:00 +0800

    Update turn_glm_intern.py

--- data/final_oneie_tradition/turn_glm_intern.py
+++ data/final_oneie_tradition/turn_glm_intern.py
@@ -1,35 +1,45 @@
 import json
 import os
 
-filename="ee_nl_zh_train_seq_oneie.json"
-f=open("./final_oneie_tradition/"+filename,"r")
 
-data=json.load(f)
-f.close()
+filelist=["ee_nl_zh_train_seq_oneie.json"
+          "ee_nl_zh_dev_seq_oneie.json"
+          "ee_nl_zh_test_seq_oneie.json"]
+
+for filename in filelist:
+  
+  f=open("./final_oneie_tradition/"+filename,"r")
+
+  data=json.load(f)
+  f.close()
 
 
-internLM=[]
+  internLM=[]
 
-for index, i in enumerate(data):
-    single=dict()
-    
-    single["id"]=str(index)
-    single["image"]=[ os.getcwd()+i["img"] ]
-    
-    single["conversations"]=[
-        {
-          "from": "user",
-          "value": "<ImageHere> "+ i["prompt"]
-        },
-        {
-          "from": "assistant",
-          "value": i["label"]
-        }
-      ]
-    
-    internLM.append(single)
-    
-f=open(os.getcwd()+"/final_oneie_tradition/"+filename.split(".")[0] +"_internLM.json","w")
-    
-json.dump(internLM,f,indent=4,ensure_ascii=False)
+  for index, i in enumerate(data):
+      single=dict()
+      
+      single["id"]=str(index)
+      single["image"]=[ os.getcwd()+"/"+i["img"] ]
+      
+      single["conversations"]=[
+          {
+            "from": "user",
+            "value": "<ImageHere> "+ i["prompt"]
+          },
+          {
+            "from": "assistant",
+            "value": i["label"]
+          }
+        ]
+      
+      internLM.append(single)
+      
+  f=open(os.getcwd()+"/final_oneie_tradition/"+filename.split(".")[0] +"_internLM.json","w")
+      
+  json.dump(internLM,f,indent=4,ensure_ascii=False)
+  f.close()
+  
+f= open("./final_oneie_tradition/ace05.txt")
+f.write( os.getcwd()+"/final_oneie_tradition/ee_nl_zh_train_seq_oneie_internLM.json 7\n")
 f.close()
